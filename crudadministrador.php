76d92a49099c1058792ff2468d262fb806e1b9ce
Patricio Bustamante <t-erence@hotmail.com>
2024-06-25 20:46:07 -0400

    ingresar administrador

---
+++ crudadministrador.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro de Administrador</title>
+</head>
+<body>
+    <h2>Registro</h2>
+    <form action="ingresaradministrador.php" method="POST">
+        Nombre: <input type="text" name="nombre" required><br>
+        Rut: <input type="text" name="rut" placeholder="Rut, Ej:11.111.111-1" required><br>
+        Contraseña: <input type="contrasena" name="contrasena" required><br>
+        <button type="submit">Registrarse</button>
+    </form>
+</body>
+</html>
