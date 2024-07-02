2162f827653aef2abbd40bdefc441d0586fd0f6a
felipe Cid <f.rcidlara@gmail.com>
2024-07-02 14:32:27 -0400

    css global y conexiones

--- crudlogincliente.php
+++ crudlogincliente.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="css/modal.css">
     <title>Inicio de Sesión</title>
 </head>
 <body>
-    <form method="POST" action="logincliente.php">
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-        <br>
-        <label for="contrasena">Contraseña:</label>
-        <input type="password" name="contrasena" required>
-        <br>
-        <input type="submit" value="Iniciar Sesión">
-    </form>
-    <p><a href="crudrecuperarcontrasena.php">¿Olvidaste tu contraseña?</a></p>
+    <div class="modal">
+        <div class="modal-contenido">
+            <form method="POST" action="logincliente.php">
+                <label for="email">Email:</label>
+                <input type="email" name="email" required>
+                <br>
+                <label for="contrasena">Contraseña:</label>
+                <input type="password" name="contrasena" required>
+                <br>
+                <button type="submit" value="Iniciar Sesión">Iniciar sesión</button>
+            </form>
+            <p><a href="crudrecuperarcontrasena.php">¿Olvidaste tu contraseña?</a></p>
+            <p><a href="indexempleado.php">ingresar como empleado</a></p>
+        </div>
+    </div>
 </body>
 </html>
