7d50aaa9e7ace398b957ceb93eb2ba39adedd108
Patricio Bustamante <t-erence@hotmail.com>
2024-07-06 12:59:53 -0400

    modificacion index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Don Perico</title>
+    <link rel="icon" href="img/logo2.png" type="image/png">
     <link rel="stylesheet" href="css/global.css">
 </head>
 <body>
