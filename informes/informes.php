274de4352b8aca1c05fd3db6ea1b0c99fcadb222
Patricio Bustamante <t-erence@hotmail.com>
2024-07-13 03:17:53 -0400

    informes.php

--- informes/informes.php
+++ informes/informes.php
@@ -2,7 +2,9 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informe de Ventas e Inventario</title>
+    <link rel="icon" href="../img/logo2.png" type="image/png">
     <link rel="stylesheet" href="../css/global.css"> <!-- Asegúrate de tener el archivo CSS adecuado -->
     <style>
         h2 {
